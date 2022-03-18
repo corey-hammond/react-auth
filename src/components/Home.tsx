a82d5b2fe96f886825e33cf252a0c8c082849cd8
Corey Hammond <coreyjhammond@gmail.com>
2022-03-17 17:51:11 -0700

    adding axios interceptors

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,20 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export const Home = () => {
+  const [message, setMessage] = useState("You are not logged in");
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("http://localhost:8000/api/user");
+
+      setMessage(`Hi ${data.first_name} ${data.last_name}`);
+    })();
+  }, []);
+
   return (
     <main className="container text-center mt-5">
-      <h1>React Auth</h1>
+      <h3>{message}</h3>
     </main>
   );
 };
